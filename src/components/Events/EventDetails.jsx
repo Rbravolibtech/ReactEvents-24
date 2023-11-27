49e341ea61f629d8c27e53d5cde5bee8764279f6
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-11-27 11:47:48 -0700

    repeating mutation concepts

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -2,31 +2,48 @@ import { Link, Outlet, useNavigate, useParams } from 'react-router-dom';
 import { useQuery, useMutation } from "@tanstack/react-query"
 
 import Header from '../Header.jsx';
-import { fetchEvent,deleteEvent,queryClient } from "../../Utility/http.js"
+import { fetchEvent, deleteEvent, queryClient } from "../../Utility/http.js"
 import ErrorBlock from '../UI/ErrorBlock.jsx';
+import { useState } from "react"
+import Modal from "../UI/Modal.jsx"
 
 export default function EventDetails() {
-  const params = useParams()
-const navigate = useNavigate()
+  const [isDeleting, setIsDeleting] = useState(false)
 
+  const params = useParams()
+  const navigate = useNavigate()
 
   const { data, isPending, isError, error } = useQuery({
     queryKey: ["events", params.id],
     queryFn: ({ signal }) => fetchEvent({ signal, id: params.id })
   })
 
-  const {mutate} = useMutation({
+  const {
+    mutate,
+    isPending: isPendingDeletion,
+    isError: isErrorDeleting,
+    error: deleteError,
+  } = useMutation({
     mutationFn: deleteEvent,
     onSuccess: () => {
-queryClient.invalidateQueries({
-  queryKey: ["events"]
-})
-navigate("/events")
+      queryClient.invalidateQueries({
+        queryKey: ["events"],
+        refetchType: "none"
+      })
+      navigate("/events")
     }
   })
 
+  function handleStartDelete() {
+    setIsDeleting(true);
+  }
+
+  function handleStopDelete() {
+    setIsDeleting(false);
+  }
+
   function handleDelete() {
-    mutate({ id: params.id})
+    mutate({ id: params.id })
   }
 
   let content;
@@ -58,8 +75,7 @@ navigate("/events")
       day: "numeric",
       month: "short",
       year: "numeric"
-    }) 
-
+    })
 
     content = (
       <>
@@ -78,7 +94,7 @@ navigate("/events")
               <p id="event-details-location">{data.location}</p>
               <time dateTime={`To-DateT$Todo-Time`}>
                 {formattedDate} @ {data.time}
-                </time>
+              </time>
             </div>
             <p id="event-details-description">{data.description}</p>
           </div>
@@ -89,6 +105,23 @@ navigate("/events")
 
   return (
     <>
+      {isDeleting && (
+        <Modal onClose={handleStopDelete}>
+          <h2>ARE YOU SURE?</h2>
+          <p>DO YOUR REALLY WANT TO DELETE THIS EVENT, THIS CAN NOT BE UNDONE.</p>
+          <div className='form-actions'>
+            {isPendingDeletion && <p>DELETING, PLEASE WAIT...</p>}
+            {isPendingDeletion && (
+              <>
+                <button onClick={handleStopDelete} className="button-text">CANCEL</button>
+                <button onClick={handleDelete} className="button">DELETE</button>
+              </>
+            )}
+          </div>
+
+          {isErrorDeleting && <ErrorBlock title="failed to delete" message={deleteError.info.message || 'failed to delete event, please try again later'} />}
+        </Modal>
+      )}
       <Outlet />
       <Header>
         <Link to="/events" className="nav-item">
